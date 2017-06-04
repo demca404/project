ea828a4a0a9325f2d9078116816676d3fb38c4b0
demca404 <demonica_@mail.ru>
2017-06-05 02:58:42 +0600

    fourth commit

--- public_dev/js/custom/script.js
+++ public_dev/js/custom/script.js
@@ -1,16 +1,52 @@
 $(document).ready(function () {
-    $(".arrow_triangle").click(function () {
-        $(".arrow_triangle").css({
+    $(".flex_question").click(function () {
+        $(".flex_question").find(".arrow_triangle").removeClass('active').css("transform", "rotate(0deg)").next().slideUp('fast');
+        $(this).find(".arrow_triangle").css({
             transform: 'rotate(90deg)'
         });
-        $(".answer").slideToggle(300);
-        $(".field").show();
-    });
-    $(".field").click(function () {
-        $(".icon").css({
-            "background-image": 'url(/images/icon.png)'
+        $(this).find(".arrow_triangle").addClass('active');
+        $(this).find(".arrow_triangle").next().slideDown('fast');
         });
-        $(".hide_menu").slideUp(300);
-        $(".field").hide();
+
+    // $(".arrow_triangle").click(function () {
+    //     $(".arrow_triangle").removeClass('active').css("transform", "rotate(0deg)").next().slideUp('fast');
+    //     $(this).css({
+    //         transform: 'rotate(90deg)'
+    //     });
+    //     $(this).addClass('active');
+    //     $(this).next().slideDown('fast');
+    // });
+    
+    $("#slider__check-1").click(function () {
+        $(".slider__text").css({
+            display: 'none'
+        });
+        $(".t1").css({
+            display: 'block'
+        })
+    });
+    $("#slider__check-2").click(function () {
+        $(".slider__text").css({
+            display: 'none'
+        });
+        $(".t2").css({
+            display: 'block'
+        })
+    });
+    $("#slider__check-3").click(function () {
+        $(".slider__text").css({
+            display: 'none'
+        });
+        $(".t3").css({
+            display: 'block'
+        })
+    });
+    $("#slider__check-4").click(function () {
+        $(".slider__text").css({
+            display: 'none'
+        });
+        $(".t4").css({
+            display: 'block'
+        })
     });
 });
